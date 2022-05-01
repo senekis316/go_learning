bef1c6cf40f27bf328a0e131745053f3a90551c8
郁方兴 <yufangxing@yufangxingdeMacBook-Pro.local>
2022-05-01 14:01:00 +0800

    modify http server

--- src/server/http_server_study.go
+++ src/server/http_server_study.go
@@ -22,7 +22,8 @@ func GetRealIP(r *http.Request) string {
 		IPAddress = r.Header.Get("X-Forwarder-For")
 	}
 	if IPAddress == "" {
-		IPAddress = r.RemoteAddr
+		// r.RemoteAddr = host:port
+		IPAddress = strings.Split(r.RemoteAddr, ":")[0]
 	}
 	return IPAddress
 }
@@ -41,14 +42,25 @@ func logRequestHandler(h http.Handler) http.Handler {
 	return http.HandlerFunc(fn)
 }
 
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, cloudnative"))
+
+	for k, v := range r.Header {
+		for _, vv := range v {
+			fmt.Println(k, v)
+			w.Header().Set(k, vv)
+		}
+	}
+}
+
 func main() {
 
 	os.Setenv("VERSION", "1.0")
 
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", index)
 	mux.HandleFunc("/healthz", healthz)
-	err := http.ListenAndServe(":8000", logRequestHandler(mux))
-	if err != nil {
+	if err := http.ListenAndServe(":8080", logRequestHandler(mux)); err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println("Http服务器开始监听")
